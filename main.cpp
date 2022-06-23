a07ca09f40dd6720c5f20b3be97f24bc0945653a
Sorshi <damian.fuchler@gmail.com>
2022-06-23 20:25:06 +0200

    Test error handling

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int arc, char * argv[]) {
     FILE* f = fopen(argv[1], "rb");
 
     if (f == nullptr) {
-        printf("\nError when openning file");
+        printf("Error when openning file");
         return 0;
     }
 
